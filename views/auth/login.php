b835111f17e198dc705f31957c1154221ef5bd53
samperDAW <asampersmrt12.alcazar@gmail.com>
2022-06-13 11:40:49 +0200

    Formulario inicio sesion

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,27 @@
-<h1>Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y Administras tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+
+        <form class="formulario" action="/" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" id="email" placeholder="Tu Email" name="email"/>
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tu password" name="password"/>
+            </div>
+            <input type="submit" class="boton" value="iniciar sesion">
+        </form>
+
+        <div class="acciones">
+              <a href="/crear">Crear Cuenta</a>  
+              <a href="/recuperar">Recuperar Contraseña</a>
+        </div>
+    </div><!--,contenedor-sm-->
+</div>
